b162707e094742ce9ba86eaede9bf68a5ec33caa
anuragCHAUHAN <118079048+AnuragCHAUHAN1@users.noreply.github.com>
2025-12-05 06:40:33 +0530

    Update main-entry.jsx

--- src/main-entry.jsx
+++ src/main-entry.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './portfolio.jsx' // Make sure this matches the filename of your Portfolio code
+import App from './App.jsx' // Make sure this matches the filename of your Portfolio code
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
